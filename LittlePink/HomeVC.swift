bb32999830ca37bba2e955174adfa1cf94141e9a
AragornCheng <aragorn.developer@outlook.com>
2024-04-29 07:37:00 +0800

    init

---
+++ LittlePink/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  LittlePink
+//
+//  Created by Aragorn Cheng on 2024/4/28.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class HomeVC: ButtonBarPagerTabStripViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
